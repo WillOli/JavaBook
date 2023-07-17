fcf511104962f1121768bee486a535f27b31057c
WillOli <william.ika@gmail.com>
2023-07-17 11:44:44 -0300

    first commit

---
+++ MeuPrimeiroPrograma/MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package MeuPrimeiroPrograma;
+
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("O primeiro de muitos códigos escrutis em " +args[0] + " pelo " + args[1] + "!");
+    }
+}
---
+++ calculadoradeestoque/Teste_exemplo_numero.java
@@ -0,0 +1,13 @@
+package calculadoradeestoque;
+
+public class Teste_exemplo_numero {
+    public static void main(String[] args) {
+        int numero = 4;
+        int outroNumeroIgual = numero;
+
+        numero = numero + 5;
+
+        System.out.println(numero);
+        System.out.print(outroNumeroIgual);
+    }
+}
