feae6281d7855932f5e9bff3b632aff1ba5af9a2
WillOli <william.ika@gmail.com>
2023-07-20 09:49:21 -0300

    Testando a classe RegraDeDesconto para o ebook

--- ObjLivro/src/main/java/com/willdev/Autor.java
+++ ObjLivro/src/main/java/com/willdev/Autor.java
@@ -31,8 +31,8 @@ public class Autor {
 
     void mostraDetalhes() {
         System.out.println("Mostrando detalhes do autor ");
-        System.out.println("Nome " + nome);
-        System.out.println("Email " + email);
-        System.out.println("CPF " + cpf);
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("CPF: " + cpf);
     }
 }
--- ObjLivro/src/main/java/com/willdev/RegrasDeDesconto.java
+++ ObjLivro/src/main/java/com/willdev/RegrasDeDesconto.java
@@ -4,6 +4,8 @@ public class RegrasDeDesconto {
     public static void main(String[] args) {
        Autor autor = new Autor();
        autor.setNome("William Oliveira");
+       autor.setCpf("000.222.000.11");
+       autor.setEmail("william@gmail.com");
 
        /*Associando o autor ao livro*/
        Livro livro = new Livro(autor);
@@ -16,9 +18,10 @@ public class RegrasDeDesconto {
        }
 
        Ebook ebook = new Ebook(autor);
+       autor.mostraDetalhes();
        ebook.setValor(29.90);
 
-       if (!ebook.aplicaDescontoDe(0.3)) {
+       if (!ebook.aplicaDescontoDe(0.51)) {
            System.out.println("Desconto no ebook não pode ser maior do que 15%");
        } else {
            System.out.println("Valor do ebook com desconto: " + ebook.getValor());
