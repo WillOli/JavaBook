035d88c104185c2b247adee99bc1814cae24a239
WillOli <william.ika@gmail.com>
2023-07-20 12:24:20 -0300

    Criando a classe Revista

---
+++ ObjLivro/src/main/java/com/willdev/Revista.java
@@ -0,0 +1,29 @@
+package com.willdev;
+
+public class Revista {
+
+    private String nome;
+    private String descricao;
+    private double valor;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+}
