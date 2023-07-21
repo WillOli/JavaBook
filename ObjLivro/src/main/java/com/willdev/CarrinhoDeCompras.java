831e190238f07fe1fa2860a900f14723eeb3a698
WillOli <william.ika@gmail.com>
2023-07-21 08:54:19 -0300

    Adicionando um array na classe CarrinhoDeCompras e instanciando um contador

--- ObjLivro/src/main/java/com/willdev/CarrinhoDeCompras.java
+++ ObjLivro/src/main/java/com/willdev/CarrinhoDeCompras.java
@@ -7,10 +7,8 @@ import com.willdev.livraria.produtos.Revista;
 public class CarrinhoDeCompras {
 
     private double total;
-    private Produto produto1;
-    private Produto produto2;
-    private Produto produto3;
-    private Produto produto4;
+    private Produto[] produtos = new Produto[10];
+    private int contador = 0;
 
     public void adiciona(Livro livro) {
         System.out.println("Adicionando: " + livro);
@@ -26,20 +24,9 @@ public class CarrinhoDeCompras {
 
     public void adiciona(Produto produto) {
         System.out.println("Adicionando: " + produto);
-
-        if (this.produto1 != null) {
-            this.produto1 = produto;
-        } else if (this.produto2 != null) {
-            this.produto2 = produto;
-        } else if (this.produto3 !=null) {
-            this.produto3 = produto;
-        } else if (this.produto4 != null) {
-            this.produto4 = produto;
-        } else {
-            System.out.println("Não tem mais espaços");
-            return;
-        }
-
+        this.produtos[contador] = produto;
+        contador ++;
+        this.total += produto.getValor();
         total += produto.getValor();
     }
     public double getTotal() {
